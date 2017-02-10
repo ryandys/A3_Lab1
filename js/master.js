4187bac5750987a7474d4d0312299c1383c7fe58
ryandys <ryan@ourexpressionstudio.com>
2017-02-10 16:19:17 -0500

    completed assignment

---
+++ js/master.js
@@ -0,0 +1,17 @@
+(function() {
+	$('.thumbInfo img').on('click', function() {
+		$.getJSON('includes/ajaxQuery.php', { model : this.id}, function(data) {
+			//console.log(data);
+			
+			$('.modelName').text(data.modelName);
+			$('.priceInfo').text(data.pricing);
+			$('.modelDetails').text(data.modelDetails);
+			$('.subhead span').text(data.model);
+			//$('.thumbInfo img').addClass('nonActive');
+			//this.classList.remove('nonActive');
+			//this.classList.add('focusMini');
+			
+			//this should be fading the images on click but its not................		
+		})
+	})
+})();
